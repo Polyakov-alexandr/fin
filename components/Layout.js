f10d117765140e2d9413998d377f6802f2248064
Polyakov-alexandr <palachhots@mail.ru>
2022-06-13 00:15:13 +0500

    login3

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => (
     <>
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet" />
             <link href="fonts/stylesheet.css" rel="stylesheet" />
         </Head>
